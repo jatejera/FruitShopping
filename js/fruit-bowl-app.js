b25f19340e22e3b738fc70bd779d07875f31f4f4
Julian A Tejera <jats.tejera@gmail.com>
2017-10-19 12:57:20 -0400

    first

---
+++ js/fruit-bowl-app.js
@@ -0,0 +1,9 @@
+[
+	{"fruit": "Apple", "Price": "5.79"},
+	{"fruit": "Banana", "Price": "7.79"},
+	{"fruit": "Mango", "Price": "5.59"},
+	{"fruit": "Orange", "Price": "2.79"},
+	{"fruit": "Peach", "Price": "3.99"},
+	{"fruit": "Pear", "Price": "6.49"},
+	{"fruit": "Plum", "Price": "3.79"}
+]
